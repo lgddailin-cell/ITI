f81b5b1360569a1bba80b025cd9a4366448cf0f7
lgddailin-cell <lgddailin@gmail.com>
2026-01-29 16:57:48 +0800

    Delete codes directory

--- codes/cuto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#JSUB -q gpu
-#JSUB -m gpu03
-#JSUB -n 10
-#JSUB -e error.%J
-
-source /apps/software/anaconda3/etc/profile.d/conda.sh
-conda activate pytorch1.12-cuda11.3
-python run.py --model ITI_ComplEx --house_dim 4 --house_num 4 --types_num 2 --housd_num 2 --batch_size 200 --negative_sample_size 400 --save_path '/home/25171213997/ITI/FB15k/ITI_ComplEx/' --valid_steps 10000 --test_log_steps 500 --save_checkpoint_steps 20000 --hidden_dim 1000 --test_batch_size 4 --data_path '/home/25171213997/ITI/data/FB15k/'
